ac2e3e8e3a85a07f4ccb870bfe75be059de4ab17
couragemarcus <courageaddae055@gmail.com>
2026-09-15 09:52:34 +0000

    even.py issues solved and better understood

--- Advance_python/even.py
+++ Advance_python/even.py
@@ -3,4 +3,13 @@ num= [1,56,234,87,4,76,24,69,90,135]
 def is_even(num):
     print("The original numbers are:", num)
     value = filter(lambda num: num % 2 == 0, num)
-print("The even numbers are:", list(value))
+    print("The even numbers are:", list(value))
+
+is_even(num)
+
+def is_odd(num):
+    print("The original numbers are:", num)
+    value = filter(lambda num:num % 2 != 0, num)
+    print("The odd numbers are:", list(value))
+
+is_odd(num)
